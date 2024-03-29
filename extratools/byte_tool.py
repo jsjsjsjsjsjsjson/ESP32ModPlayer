f2a38fcb8100957c566a90d0d59cb274dd247c2c
libchara <libchara@libcharadeMBP.lan>
2024-03-29 15:14:57 +0800

    Drop Linear Interpolation and fix some bug

--- extratools/byte_tool.py
+++ extratools/byte_tool.py
@@ -14,7 +14,7 @@ def file_to_c_uint8_array(input_file, output_file, array_name):
         f.write("\n};\n")
 
 if __name__ == "__main__":
-    input_file = "d-zire_-_native_cry.mod"
-    output_file = "d-zire_-_native_cry.h"
+    input_file = "space_debris.mod"
+    output_file = "space_debris.h"
     array_name = "tracker_data"
     file_to_c_uint8_array(input_file, output_file, array_name)
